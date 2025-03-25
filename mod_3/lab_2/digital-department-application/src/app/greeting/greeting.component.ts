26794ac833bef02aae950194ea446b873f33cdbd
Marina <nozdryakovamarina94@gmail.com>
2025-03-25 21:00:36 +0400

    lab_ 2 complete feat: Implement life cycles

--- mod_3/lab_2/digital-department-application/src/app/greeting/greeting.component.ts
+++ mod_3/lab_2/digital-department-application/src/app/greeting/greeting.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -7,8 +7,43 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './greeting.component.html',
   styleUrl: './greeting.component.css'
 })
-export class GreetingComponent {
+export class GreetingComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, 
+AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
+
+  ngOnChanges(changes: SimpleChanges): void {
+     console.log(changes);
+  }
+
+  ngOnInit(): void {
+     console.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+     console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+     console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+     console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+     console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+     console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+     console.log('ngOnDestroy');
+  }
+
   name: any
+
   sayHi(): void {
     alert("Привет, " + this.name);
   }
